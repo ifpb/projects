baed199edd9d79d8930e3a6bc619114f7f5bbd82
Luiz Carlos <lucachaves@gmail.com>
2026-08-27 10:00:56 -0300

    refactor: enhance Filter component layout and accessibility in BaseLayout

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -35,20 +35,28 @@ const Filter = React.memo(function Filter(props: FilterProps) {
   }, []);
 
   return (
-    <div className="relative">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className="absolute right-0 mr-[10%] md:mr-32 lg:mr-32 xl:mr-0 text-4xl cursor-pointer mt-0.5"
+    <div className="relative inline-block">
+      <button
+        type="button"
         onClick={toggleShow}
         onMouseEnter={preloadDrawer}
         onTouchStart={preloadDrawer}
+        className="inline-flex items-center gap-2 px-4 py-2 rounded-full bg-white text-gray-700 font-semibold text-sm border border-gray-300 shadow-xs hover:bg-gray-50 hover:text-gray-900 hover:border-gray-400 focus:outline-hidden focus:ring-2 focus:ring-gray-400 transition-all cursor-pointer"
+        aria-label="Abrir menu de filtros"
+        aria-expanded={isShow}
       >
-        <path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z" />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1.1em"
+          height="1.1em"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="text-gray-600"
+        >
+          <path d="M4.25 5.61C3.27 4.35 4.17 2.5 5.76 2.5h12.48c1.59 0 2.49 1.85 1.51 3.11l-5.75 7.39v5.75c0 .41-.17.82-.47 1.12l-2 2c-.63.63-1.71.18-1.71-.71v-8.16L4.25 5.61z" />
+        </svg>
+        <span>Filtros</span>
+      </button>
 
       {isShow && (
         <Suspense fallback={null}>
